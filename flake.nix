4dba3733d47810dc4caa25aa0d5b584e4e4a9b6c
qrxnz <send@qrxnz.dev>
2024-11-12 18:13:08 +0100

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "stego-toolkit";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.x86_64-linux.default =
+      pkgs.mkShell
+      {
+        nativeBuildInputs = with pkgs; [
+          python311Full
+          python311Packages.pycryptodomex
+        ];
+      };
+  };
+}
+
