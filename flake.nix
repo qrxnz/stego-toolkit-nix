c63832e6e29a5f9261651acf1b59e6efff007cdb
qrxnz <send@qrxnz.dev>
2024-12-20 10:46:26 +0100

    Flake update

--- flake.nix
+++ flake.nix
@@ -1,38 +1,10 @@
 {
-  description = "A collection of flake templates";
-
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        treefmt2
-        mdformat
-        alejandra
-      ];
-    };
-
+  outputs = {self}: {
     templates = {
       stego-toolkit = {
         path = ./stego-toolkit;
-        welcomeText = ''
-
-
-           ▄▀▀▀▀▄  ▄▀▀▀█▀▀▄  ▄▀▀█▄▄▄▄  ▄▀▀▀▀▄    ▄▀▀▀▀▄   ▄▀▀▀█▀▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀▀▀▄      ▄▀▀▄ █  ▄▀▀█▀▄    ▄▀▀▀█▀▀▄
-          █ █   ▐ █    █  ▐ ▐  ▄▀   ▐ █         █      █ █    █  ▐ █      █ █      █ █    █      █  █ ▄▀ █   █  █  █    █  ▐
-             ▀▄   ▐   █       █▄▄▄▄▄  █    ▀▄▄  █      █ ▐   █     █      █ █      █ ▐    █      ▐  █▀▄  ▐   █  ▐  ▐   █
-          ▀▄   █     █        █    ▌  █     █ █ ▀▄    ▄▀    █      ▀▄    ▄▀ ▀▄    ▄▀     █         █   █     █        █
-           █▀▀▀    ▄▀        ▄▀▄▄▄▄   ▐▀▄▄▄▄▀ ▐   ▀▀▀▀    ▄▀         ▀▀▀▀     ▀▀▀▀     ▄▀▄▄▄▄▄▄▀ ▄▀   █   ▄▀▀▀▀▀▄   ▄▀
-           ▐      █          █    ▐   ▐                  █                             █         █    ▐  █       █ █
-                  ▐          ▐                           ▐                             ▐         ▐       ▐       ▐ ▐
-        '';
       };
 
-      defaultTemplate = self.templates.stego-toolkit;
-    };
+    defaultTemplate = self.templates.flakeonly;
   };
 }
